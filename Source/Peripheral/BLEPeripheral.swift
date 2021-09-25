5482e3526f576ee271c57f9a6b45dd23f32b6a78
Henryforce <hj_serrano@hotmail.com>
2021-09-25 17:00:57 +0800

    Updated changes from PR 17

--- Source/Peripheral/BLEPeripheral.swift
+++ Source/Peripheral/BLEPeripheral.swift
@@ -16,7 +16,6 @@ public protocol BLEPeripheral {
     func observeConnectionState() -> AnyPublisher<Bool, Never>
     func connect(with options: [String: Any]?) -> AnyPublisher<BLEPeripheral, BLEError>
     @discardableResult func disconnect() -> AnyPublisher<Never, BLEError>
-    @discardableResult func disconnect() -> AnyPublisher<Bool, BLEError>
     func observeNameValue() -> AnyPublisher<String, Never>
     func observeRSSIValue() -> AnyPublisher<NSNumber, BLEError>
     func discoverServices(serviceUUIDs: [CBUUID]?) -> AnyPublisher<BLEService, BLEError>
